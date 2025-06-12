b308332b84c1c15e7431a4e5617f29f0d0e97adc
Carla <CarlaCornejoG23@hotmail.com>
2025-06-12 17:33:57 -0400

    auth forms in components

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,60 +1,22 @@
-"use client";
-import Link from "next/link";
-import { useState, FormEvent } from "react";
+import ForgotPasswordForm from "@/src/components/auth/forgotPasswordForm";
 
-export default function forgotPassword(){
-
-    // Estado para manejar el correo electrónico
-    const [email, setEmail] = useState("");
-
-    //Simulación de correos electrónicos registrados
-    const registeredEmails = ["admin@correo.com", "usuario@ejemplo.com"];
-
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        if (!email) {
-            alert("Por favor, ingrese un correo electrónico.");
-            return;
-        }
-
-        const emailRegex = /^\S+@\S+\.\S+$/;
-        if (!emailRegex.test(email)) {
-            alert("Por favor, ingrese un correo válido.");
-            return;
-        }
-
-        if (registeredEmails.includes(email)) {
-            alert("Hemos enviado un correo a " + email + " con las instrucciones para restablecer la contraseña.");
-        } else {
-            alert("El correo no está registrado.");
-        }
-    };
+function forgotPassword(){
 
     return (
         <div className="min-h-screen flex flex-col justify-center rounded">
             <div className="max-w-md w-full mx-auto mt-4 p-8 border border-gray-300 rounded-3xl" style={{ backgroundColor: '#E8E8E8' }}>
 
-                <img src="/next.svg" alt="Logo" className="mx-auto mb-6 w-32 h-auto" />
+                <img src="/images/dicase-logo-negro.png" alt="Logo" className="mx-auto mb-1 w-60 h-auto" />
 
                 <div className="text-center font-bold text-2xl">Recuperar Contraseña</div>
 
                 <div className="mt-5 text-sm text-gray-600">Ingresa tu correo electrónico y te enviaremos las instrucciones para una nueva contraseña.</div>
 
-                <form onSubmit={handleSubmit} className="space-y-4 mt-4" noValidate>
-                    <div>
-                        <input type="email" placeholder="Correo Electrónico" className="w-full p-2 border rounded mt-1" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-
-                    <div className="text-center">
-                        <button type="submit" className="py-2 px-4 bg-black text-white hover:bg-gray-800 rounded-md text-bold">Recuperar Contraseña</button>
-                    </div>
-
-                    <div className="text-center font-bold">
-                        <Link href="/auth/login" className="font-bold underline">Atrás para iniciar sesión</Link>
-                    </div>
-                </form>
+                <ForgotPasswordForm />
             </div>
         </div>
         
     );
 }
+
+export default forgotPassword;
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,88 +1,21 @@
-"use client";
-import { useState, FormEvent, ChangeEvent } from "react";
-import Link from "next/link";
+import LoginForm from "@/src/components/auth/loginForm";
 
-export default function login(){
-
-    // Estados para manejar los campos del formulario y errores
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState<{email?: string, password?: string}>({});
-
-    // Manejadores de eventos para los campos del formulario
-    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-        setErrors(prev => ({ ...prev, email: undefined }));
-    };
-
-    const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-        setErrors(prev => ({ ...prev, password: undefined }));
-    };
-
-
-    // Validación de campos
-    const validate = () => {
-        const newErrors: { email?: string, password?: string } = {};
-        if (!email) {
-            newErrors.email = "El correo electrónico es obligatorio.";
-        } else if (!/\S+@\S+\.\S+/.test(email)) {
-            newErrors.email = "El correo electrónico no es válido.";
-        }
-        if (!password) {
-            newErrors.password = "La contraseña es obligatoria.";
-        } else if (password.length < 8) {
-            newErrors.password = "La contraseña debe tener al menos 8 caracteres.";
-        }
-        setErrors(newErrors);
-        return Object.keys(newErrors).length === 0;
-    };
-
-    // Manejo del envío del formulario (Temporalmente en simulación hasta tener un backend)
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-        if (validate()) {
-            if (email === "admin@correo.com" && password === "12345678") {
-            alert("Login exitoso");
-            } else {
-            alert("Email o contraseña incorrectos");
-            }
-        }
-    };
+function Login(){
 
     return (
         <div className="min-h-screen flex flex-col justify-center rounded">
             <div className="max-w-md w-full mx-auto mt-4 p-8 border border-gray-300 rounded-3xl" style={{ backgroundColor: '#E8E8E8' }}>
 
-                <img src="/next.svg" alt="Logo" className="mx-auto mb-6 w-32 h-auto" />
+                <img src="/images/dicase-logo-negro.png" alt="Logo" className="mx-auto mb-1 w-60 h-auto" />
 
                 <div className="text-center font-bold text-2xl">Inicia sesión en Dicase</div>
 
-                <form onSubmit={handleSubmit} className="space-y-6 mt-10" noValidate>
-                    <div>
-                        <input type="email" placeholder="Correo Electrónico" className={`w-full p-2 border rounded mt-1 ${errors.email ? "border-red-500" : "border-gray-400"}`} value={email} onChange={handleEmailChange} />
-                        {errors.email && (<p className="text-red-600 text-sm mt-1">{errors.email}</p>)}
-                    </div>
+                <LoginForm />
 
-                    <div>
-                        <input type="password" placeholder="Contraseña" className={`w-full p-2 border rounded mt-1 ${errors.password ? "border-red-500" : "border-gray-400"}`} value={password} onChange={handlePasswordChange} />
-                        {errors.password && (<p className="text-red-600 text-sm mt-1">{errors.password}</p>)}
-                    </div>
-
-                    <div className="text-center">
-                        <Link href="/auth/forgot-password" className="font-bold underline">¿Has olvidado tu contraseña?</Link>
-                    </div>
-
-                    <div className="text-center">
-                        <button type="submit" className="py-2 px-4 bg-black text-white hover:bg-gray-800 rounded-md text-bold">Iniciar Sesión</button>
-                    </div>
-                    
-                    <div className="text-center font-bold">
-                        ¿No tienes una cuenta? {''} <Link href="/auth/register" className="font-bold underline">Regístrate aquí</Link>
-                    </div>
-                </form>
             </div>
         </div>
-        
+
     );
 }
+
+export default Login;
